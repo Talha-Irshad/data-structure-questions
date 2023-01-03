5bac835805ca6db07437d5de673fb75ea2b37a04
Talha-Irshad <60492493+Talha-Irshad@users.noreply.github.com>
2023-01-03 20:07:19 +0500

    1 file modified

--- index.js
+++ index.js
@@ -26,4 +26,20 @@ const isBalanced = (str) => {
   }
 };
 
-isBalanced(inpStr);
+// isBalanced(inpStr);
+
+const getSecondLargest = (arr) => {
+  let largest = arr[0];
+  let secondLargest = arr[0];
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] > largest) {
+      secondLargest = largest;
+      largest = arr[i];
+    } else if (arr[i] > secondLargest) {
+      secondLargest = arr[i];
+    }
+  }
+  console.log('second largest: ', secondLargest);
+};
+
+getSecondLargest([10, 122, 12, 1444, 100]);
